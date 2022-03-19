873af487557416828720ff48d7c846d8d2dd4fda
Xuan-Yu Lin <kim4501@gmail.com>
2022-03-19 20:59:58 +0800

    done

--- GEOFENCE/GEOFENCE.sim/sim_1/behav/xsim/xsim.dir/testfixture_behav/webtalk/xsim_webtalk.tcl
+++ GEOFENCE/GEOFENCE.sim/sim_1/behav/xsim/xsim.dir/testfixture_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/kim09/IC_contest/GEOFENCE/GEOFENCE/GEOFENCE.sim/sim_1/behav/xsim/xsim.dir/testfixture_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Sat Mar 19 20:37:58 2022" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sat Mar 19 20:58:01 2022" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2258646" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "fc69124c-8c15-48f9-93a3-7e36cd615f9c" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "09995c89dba9413eb87e6baa554bdea6" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "9" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "8" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-7700HQ CPU @ 2.80GHz" -context "user_environment"
@@ -22,11 +22,12 @@ webtalk_add_data -client project -key cpu_speed -value "2808 MHz" -context "user
 webtalk_add_data -client project -key total_processors -value "1" -context "user_environment"
 webtalk_add_data -client project -key system_ram -value "8.000 GB" -context "user_environment"
 webtalk_register_client -client xsim
+webtalk_add_data -client xsim -key runall -value "true" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "33240 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.11_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "7300_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 737146335 -regid "" -xml C:/Users/kim09/IC_contest/GEOFENCE/GEOFENCE/GEOFENCE.sim/sim_1/behav/xsim/xsim.dir/testfixture_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/kim09/IC_contest/GEOFENCE/GEOFENCE/GEOFENCE.sim/sim_1/behav/xsim/xsim.dir/testfixture_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/kim09/IC_contest/GEOFENCE/GEOFENCE/GEOFENCE.sim/sim_1/behav/xsim/xsim.dir/testfixture_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.16_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "7912_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 3892926766 -regid "" -xml C:/Users/kim09/IC_contest/GEOFENCE/GEOFENCE/GEOFENCE.sim/sim_1/behav/xsim/xsim.dir/testfixture_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/kim09/IC_contest/GEOFENCE/GEOFENCE/GEOFENCE.sim/sim_1/behav/xsim/xsim.dir/testfixture_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/kim09/IC_contest/GEOFENCE/GEOFENCE/GEOFENCE.sim/sim_1/behav/xsim/xsim.dir/testfixture_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
